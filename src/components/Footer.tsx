1ad542787cedb2d26129a424a2381c67560704a1
eli-swiftly <eli@swiftly.world>
2024-07-12 18:19:58 +0200

    Update Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-200 p-4 mt-8">
+      <p className="text-center">© 2024 My Next.js App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
